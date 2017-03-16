51559bccbe45f86a900aa7c0c56d1336fd3da24a
Keylost <roman1@tambo.ru>
2017-03-16 16:39:31 +0700

    [*] signs.hpp sync

--- sources/signs.hpp
+++ sources/signs.hpp
@@ -8,11 +8,14 @@ enum signs
 	sign_crosswalk 	   = 2,
 	sign_mainroad      = 3,
 	sign_giveway       = 4,
-	sign_starttrafficlight_green = 5,
-	sign_starttrafficlight_red = 6,
-	sign_trafficlight_green  = 7,
-	sign_trafficlight_yellow = 8,
-	sign_trafficlight_red = 9
+	sign_starttrafficlight_green 	= 5,
+	sign_starttrafficlight_red 		= 6,
+	sign_trafficlight_green  		= 7,
+	sign_trafficlight_yellow 		= 8,
+	sign_trafficlight_red 			= 9,
+	sign_trafficlight_off			= 10,
+	sign_trafficlight_yelRed  		= 11,
+	sign_starttrafficlight_off 		= 12
 };
 
 enum trafficlight_states
@@ -26,13 +29,13 @@ struct sign_data
 {
 	cv::Rect area;
 	signs sign;
-	uint32_t detect_time; //время прошедшее с поледне регистрации знака системой
+	uint32_t detect_time; //время, прошедшее с поледней регистрации знака системой
 };
 
 struct line_data
 {
-	int32_t robot_center = 323; //point on image
-	int32_t center_of_line = 323; //center of black line
+	int32_t robot_center = 323; //точка на изображении, на которую будет выравниваться робот
+	int32_t center_of_line = 323; //точка на изображении, в которой находится центр направляющей линии
 	bool on_line = true;
 	bool stop_line = false;
 };
