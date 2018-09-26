8ba1151752e4bceece038c0588915f5a1ecbd69e
Roman Peshkichev <roman1@tambo.ru>
2018-09-26 10:30:39 +0700

    [*] relayingserver for linux

--- sources/server.cpp
+++ sources/server.cpp
@@ -1,5 +1,8 @@
-#if defined(RELAYING_MODE)
 #include "server.hpp"
+
+#if defined(RELAYING_MODE)
+
+#if defined(WIN32)
 
 void server_fnc(System &syst)
 {
@@ -45,7 +48,7 @@ void server_fnc(System &syst)
 			WSACleanup();
 			return 1;
 		}
-
+socklen_t clilen;
 		// Create a SOCKET for connecting to server
 		ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
 		if (ListenSocket == INVALID_SOCKET) {
@@ -101,9 +104,9 @@ void server_fnc(System &syst)
 					std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 				}
 			}
-			syst.engine_get(localEngine);
+			syst.engisocklen_t clilen;ne_get(localEngine);
 			syst.currentJPEG_get(lastjpeg);
-			srtTosnd.angle = localEngine.angle;
+			strToSnd.angle = localEngine.angle;
 			strToSnd.speed = localEngine.real_speed;
 			lastjpegSize = (uint32_t)lastjpeg.size();
 
@@ -128,4 +131,109 @@ void server_fnc(System &syst)
 	return;
 }
 
+#else
+
+void sockOptEnable(int sockfd, int optName)
+{
+	int32_t optval = 1;
+	size_t optlen = sizeof(optval);
+	if(setsockopt(sockfd, SOL_SOCKET, optName, (char *)&optval, optlen) < 0)
+	{
+		perror("sockOptEnable()");
+		close(sockfd);
+		exit(EXIT_FAILURE);
+	}
+}
+
+bool send_data(void *src,int socket,size_t size)
+{
+	int bytes = 0;
+	size_t i = 0;
+	for (i = 0; i < size; i += bytes) 
+	{
+		if ((bytes = send(socket, ((uint8_t *)src)+i, size-i, MSG_NOSIGNAL)) <= 0) 
+		{
+			printf("Sending data error");
+			return false;
+		}
+	}
+	return true;
+}
+
+void server_fnc(System &syst)
+{
+	int portno = 1211;
+	Engine localEngine;
+	std::vector<uchar> lastjpeg;
+	uint32_t lastjpegSize = 0;
+	while(true)
+	{
+		EngineRelay strToSnd;
+		strToSnd.angle = 90; //at center by default 
+		strToSnd.speed = 20;
+		printf("Relaying server started\n");
+		socklen_t clilen;
+		int sockfd = 0;
+		int newsockfd = 0;
+		struct sockaddr_in serv_addr, cli_addr;
+		sockfd = socket(AF_INET, SOCK_STREAM, 0);
+		if (sockfd < 0)
+		{
+			printf("ERROR opening socket\n");
+		}
+		sockOptEnable(sockfd,SO_KEEPALIVE);
+		sockOptEnable(sockfd,SO_REUSEADDR);
+
+		bzero((char *) &serv_addr, sizeof(serv_addr));
+
+		serv_addr.sin_family = AF_INET;
+		serv_addr.sin_addr.s_addr = INADDR_ANY;
+		serv_addr.sin_port = htons(portno);
+			
+		while(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr))<0)
+		{
+			printf("ERROR binding socket\n");
+			sleep(1); //wait 1 sec befor next try
+		}
+
+		listen(sockfd,5);
+		clilen = sizeof(cli_addr);
+		printf("waiting for client\n");
+		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+		printf("client connected\n");
+	
+		while (true)
+		{
+			std::this_thread::sleep_for(std::chrono::milliseconds(33));
+			if(syst.getExitState())
+			{
+				close(newsockfd);
+				while(true)
+				{
+					std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+				}
+				exit(0);
+			}
+			syst.engine_get(localEngine);
+			syst.currentJPEG_get(lastjpeg);
+			strToSnd.angle = localEngine.angle;
+			strToSnd.speed = localEngine.real_speed;
+			lastjpegSize = (uint32_t)lastjpeg.size();
+			
+			if(!send_data((char *)(&strToSnd), newsockfd, sizeof(EngineRelay))) goto sockerr;
+			if(!send_data((char *)(&lastjpegSize), newsockfd, sizeof(uint32_t))) goto sockerr;
+			if(!send_data((char *)(&lastjpeg[0]), newsockfd, lastjpegSize)) goto sockerr;
+			
+			if(false)
+			{
+				sockerr:
+				printf("Restarting server...\n");
+				close(newsockfd);
+				break;
+			}
+		}
+	}
+}
+
+#endif
 #endif
--- sources/server.hpp
+++ sources/server.hpp
@@ -4,22 +4,31 @@
 #include "config.hpp"
 
 #if defined(RELAYING_MODE)
+
+#ifdef __linux__
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <sys/select.h>
+#else
 #undef UNICODE
-
 #define WIN32_LEAN_AND_MEAN
-
 #include <windows.h>
 #include <winsock2.h>
 #include <ws2tcpip.h>
+#pragma comment (lib, "Ws2_32.lib")
+// #pragma comment (lib, "Mswsock.lib")
+#endif
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <chrono>
 #include <thread>
 
-// Need to link with Ws2_32.lib
-#pragma comment (lib, "Ws2_32.lib")
-// #pragma comment (lib, "Mswsock.lib")
 
 #define DEFAULT_BUFLEN 512
 #define DEFAULT_PORT "1211"
